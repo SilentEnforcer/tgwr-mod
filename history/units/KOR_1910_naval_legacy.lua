dd1d551f8c4a45649e772b7d6ffbf4a1c439beb0
Han Myeong-Ho <thks1018@gmail.com>
2020-09-24 15:41:31 +0900

    Add Korean navy (legacy)

---
+++ history/units/KOR_1910_naval_legacy.lua
@@ -0,0 +1,26 @@
+
+units = {
+	##### NAVAL UNITS #####
+	fleet = {
+		name = "Daehanjeguk Haegun"			
+		naval_base = 7221  # Gyeonggi Jemulpo
+		task_force = {
+			name = "Gwangjeho"			
+			location = 7221  # Gyeonggi Jemulpo
+			ship = {
+				name = "Gwangjeho" 
+				definition = destroyer
+				equipment = { destroyer_1 = { amount = 1 owner = KOR } }
+			}
+		}
+		task_force = {
+			name "Yangmuho"
+			location = 4056  # Gyeongsang Busanpo
+			ship = { 
+				name = "Yangmuho" 
+				definition = destroyer
+				equipment = { destroyer_1 = { amount = 1 owner = KOR } }
+			}
+		}
+	}
+}
