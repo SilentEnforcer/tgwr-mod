8f48183387fb6dd2b6f9af2ab99e319eca2d714c
SilentEnforcer <mol.oksak@yandex.ru>
2021-05-26 22:20:12 +0300

    scandinavian kings

--- common/defines/tgwr_defines.lua
+++ common/defines/tgwr_defines.lua
@@ -7,17 +7,17 @@ NDefines.NCountry.WAR_SUPPORT_OFFNSIVE_WAR = -0.1
 
 NDefines.NDiplomacy.TENSION_TIME_SCALE_START_DATE = "1910.5.1.12"
 
-NDefines.NDiplomacy.TENSION_STATE_VALUE = 0									-- Vanilla is 2
-NDefines.NDiplomacy.TENSION_CIVIL_WAR_IMPACT = 0							-- Vanilla is 0.2
+NDefines.NDiplomacy.TENSION_STATE_VALUE = 0.1								-- Vanilla is 2
+NDefines.NDiplomacy.TENSION_CIVIL_WAR_IMPACT = 0.2							-- Vanilla is 0.2
 NDefines.NDiplomacy.TENSION_NO_CB_WAR = 10									-- Vanilla is 15
 NDefines.NDiplomacy.TENSION_CB_WAR = 5										-- Vanilla is 5
-NDefines.NDiplomacy.TENSION_ANNEX_NO_CLAIM = 0								-- Vanilla is 2
-NDefines.NDiplomacy.TENSION_ANNEX_CLAIM = 0									-- Vanilla is 1
-NDefines.NDiplomacy.TENSION_ANNEX_CORE = 0								  	-- Vanilla is 0.5
+NDefines.NDiplomacy.TENSION_ANNEX_NO_CLAIM = 0.2								-- Vanilla is 2
+NDefines.NDiplomacy.TENSION_ANNEX_CLAIM = 0.15									-- Vanilla is 1
+NDefines.NDiplomacy.TENSION_ANNEX_CORE = 0.1								  	-- Vanilla is 0.5
 NDefines.NDiplomacy.TENSION_PUPPET = 0										-- Vanilla is 2
 NDefines.NDiplomacy.TENSION_GENERATE_WARGOAL = 0.5							-- Vanilla is 1
 NDefines.NDiplomacy.TENSION_VOLUNTEER_FORCE_DIVISION = 0					-- Vanilla is 0.5
-NDefines.NDiplomacy.TENSION_SIZE_FACTOR = 0									-- Vanilla is 1.0
+NDefines.NDiplomacy.TENSION_SIZE_FACTOR = 1.0								-- Vanilla is 1.0
 
 NDefines.NAI.DIPLOMACY_FACTION_WRONG_IDEOLOGY_PENALTY = 30
 NDefines.NAI.NEUTRAL_THREAT_PARANOIA = 0									
